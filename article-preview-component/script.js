c8684873d2bd86ff53a31a6a7c1a2733bc2a0f39
Thomas Sankara <thomassankara74@gmail.com>
2020-09-02 10:48:16 +0300

    article preview component

---
+++ article-preview-component/script.js
@@ -0,0 +1,6 @@
+const share = document.querySelector('.fa-share')
+const tooltip = document.querySelector('.tootltip-inner')
+
+share.addEventListener('click', () => {
+  tootlip.style.display = 'block'
+})
