18231271a2ff799d8abfeaacf0667feffb59e874
SankThomas <thomassankara74@gmail.com>
2020-09-09 15:40:16 +0300

    pricing component with toggle - complete

---
+++ pricing-component-with-toggle/script.js
@@ -0,0 +1,13 @@
+// const checkbox = document.getElementById('checkbox')
+// const monthlyBasic = document.getElementById('monthly-basic')
+// const monthlyPro = document.getElementById('monthly-pro')
+// const monthlyMaster = document.getElementById('monthly-master')
+// const annualBasic = document.getElementById('annual-basic')
+// const annualPro = document.getElementById('annual-pro')
+// const annualMaster = document.getElementById('annual-master')
+
+// checkbox.addEventListener('change', (priceChange) => {
+//   monthlyBasic.classList.toggle('monthly')
+//   monthlyPro.classList.toggle('monthly')
+//   monthlyMaster.classList.toggle('monthly')
+// })
